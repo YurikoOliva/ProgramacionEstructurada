b659bb08d5fd30ffc121af8e06c0b6b3d8e10273
YurikoOliva <60494973+YurikoOliva@users.noreply.github.com>
2020-03-12 20:14:00 -0600

    Add files via upload

--- Practica6/dowhile.c
+++ Practica6/dowhile.c
@@ -2,23 +2,22 @@
 
 int main()
 {
-	int i=0,num=0;
+	int i=-1,num=0;
 	float prom,sum=0;
 	
 	do
 	{
+		sum=sum+num;
 		printf("\nIngrese un numero: ");
 			scanf("%d", &num);
-				sum=sum+num;
-				i++;	
-		printf("\nSuma= %.2f\n",sum);
-	
+				i++;
 	}while(num>=0);
-	    i=i-1;
-	    sum=sum-num;
-		prom=sum/i;
-		printf("\nTotal de numeros leidos: %d", i);
-		printf("\n\nEl promedio es: %.2f",prom);
+	    if(num==0)
+	    	printf("\nEl promedio no se puede hacer\n");
+	    else
+			prom=sum/i;
+			printf("\nTotal de numeros leidos: %d", i);
+			printf("\n\nEl promedio es: %.2f",prom);
 				
 	return 0;
 
